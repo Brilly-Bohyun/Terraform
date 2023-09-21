f9d6536648e7e7eb770867c049f0e6858b861525
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-21 13:57:13 +0900

    🔧 add provider configuration

--- 03.start/main.tf
+++ 03.start/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.0.0"
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = ">= 2.0.0"
+    }
+  }
 }
 
 # local_file은 테라폼의 local 프로바이더로 파일을 프로비저닝하는데 사용
