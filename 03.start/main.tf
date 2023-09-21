26ee0eec32bfc12732104960b6f69b2c7e9ac0f2
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-21 14:00:27 +0900

    🔧 check state file's lock

--- 03.start/main.tf
+++ 03.start/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 # local_file은 테라폼의 local 프로바이더로 파일을 프로비저닝하는데 사용
 resource "local_file" "abc" {
-  content = "abc!"
+  content = "123456!"
   filename = "${path.module}/abc.txt"
   # path.module은 실행되는 테라폼 모듈의 파일 시스템 경로
 }
