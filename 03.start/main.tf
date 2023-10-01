d0914f6691c1c82258c1373e6d9261f9dba77360
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-10-01 21:10:44 +0900

    🔧 add sensitive variable

--- 03.start/main.tf
+++ 03.start/main.tf
@@ -11,9 +11,14 @@ terraform {
   }
 }
 
+variable "my_password" {
+  default = "password"
+  sensitive = true
+}
+
 # local_file은 테라폼의 local 프로바이더로 파일을 프로비저닝하는데 사용
 resource "local_file" "abc" {
-  content = "123!"
+  content = var.my_password
   filename = "${path.module}/abc.txt"
   # path.module은 실행되는 테라폼 모듈의 파일 시스템 경로
 }
