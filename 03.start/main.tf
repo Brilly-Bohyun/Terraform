9949443b97604b6e6c32f74f6098150221fd911f
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-10-01 21:43:19 +0900

    🔧 add output

--- 03.start/main.tf
+++ 03.start/main.tf
@@ -30,9 +30,17 @@ locals {
 
 # local_file은 테라폼의 local 프로바이더로 파일을 프로비저닝하는데 사용
 resource "local_file" "abc" {
-  content = local.content
+  content = "abc123"
   filename = "${path.module}/abc.txt"
   # path.module은 실행되는 테라폼 모듈의 파일 시스템 경로
+}
+
+output "file_id" {
+  value = local_file.abc.id
+}
+
+output "file_abspath" {
+  value = abspath(local_file.abc.filename)
 }
 
 data "local_file" "abc" {
