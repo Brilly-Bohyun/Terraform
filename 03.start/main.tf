d565c6fd39e0038dd662fb58220e2819e86b3954
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-10-01 21:32:35 +0900

    🔧 add local

--- 03.start/main.tf
+++ 03.start/main.tf
@@ -16,9 +16,21 @@ variable "my_password" {
   sensitive = true
 }
 
+variable "my_var" {
+  default = "var2"
+}
+
+variable "prefix" {
+  default = "hello"
+}
+
+locals {
+  name = "terraform"
+}
+
 # local_file은 테라폼의 local 프로바이더로 파일을 프로비저닝하는데 사용
 resource "local_file" "abc" {
-  content = var.my_password
+  content = local.content
   filename = "${path.module}/abc.txt"
   # path.module은 실행되는 테라폼 모듈의 파일 시스템 경로
 }
