d0ce1b55321b1482d9db26c7af6d24eb7519fc6a
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-10-01 21:54:14 +0900

    🔧 add 반복문

--- 03.start/main.tf
+++ 03.start/main.tf
@@ -28,10 +28,16 @@ locals {
   name = "terraform"
 }
 
+variable "names" {
+  type = list(string)
+  default = [ "a","b","c" ]
+}
+
 # local_file은 테라폼의 local 프로바이더로 파일을 프로비저닝하는데 사용
 resource "local_file" "abc" {
+  count = length(var.names)
   content = "abc123"
-  filename = "${path.module}/abc.txt"
+  filename = "${path.module}/abc-${var.names[count.index]}.txt"
   # path.module은 실행되는 테라폼 모듈의 파일 시스템 경로
 }
 
@@ -48,6 +54,8 @@ data "local_file" "abc" {
 }
 
 resource "local_file" "def" {
-  content = data.local_file.abc.content
-  filename = "${path.module}/def.txt"
+  count = length(var.names)
+  content = local_file.abc[count.index].content
+  # element function 활용
+  filename = "${path.module}/def-${element(var.names, count.index)}.txt"
 }
